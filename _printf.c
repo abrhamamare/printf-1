e26576ef2b0ddfa6099bead997bf889e9989e8e0
1270holberton <1270@holbertonschool.com>
2019-10-28 16:03:09 +0000

    [Feature]: Cew file "_printf.c" empty for test

---
+++ _printf.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+/**
+ * _printf - Function that produces output according to a format.
+ * @format: Pointer
+ * Return: Always 0
+ */
+int _printf(const char *format, ...)
+{}
