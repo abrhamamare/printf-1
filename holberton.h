ec8fd2816db1494da92de79c9dcb18d77cdebeae
1270holberton <1270@holbertonschool.com>
2019-10-29 15:23:47 +0000

    [Feature]: Update prototypes

--- holberton.h
+++ holberton.h
@@ -1,5 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int _putchar(char c);
+int op_c(va_list al);
+int op_s(va_list al);
+int op_d(va_list al);
 int _printf(const char *format, ...);
 /**
  * struct op - Struct
