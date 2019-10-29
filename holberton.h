3445a7bedd57cadbdf6a5998b2a03b8daab8238b
1270holberton <1270@holbertonschool.com>
2019-10-29 14:23:53 +0000

    [Feature]: Update header file

--- holberton.h
+++ holberton.h
@@ -1,4 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _printf(const char *format, ...);
+/**
+ * struct op - Struct
+ * @op: input
+ * @func: function
+ */
+typedef struct op
+{
+	char *op;
+	int (*func)(va_list al);
+} op_t;
 #endif
